1679492a172072835c3d516b1f79a720c35196ca
Marcos Alexandre Rodrigues de Carvalho <45478002+mencholipa14@users.noreply.github.com>
2019-06-12 02:20:21 -0300

    Update conecta_banco.php

--- banco/conecta_banco.php
+++ banco/conecta_banco.php
@@ -1,7 +1,7 @@
 <?php 
 $host = "localhost";
 $user = "root";
-$pass = "usbw";
+$pass = "usbw"; // Senha usada em um servidor UsbWebServer
 $db = "uau_chat";
 $con = new Mysqli($host, $user, $pass, $db);
 ?>
